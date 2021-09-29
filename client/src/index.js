dd14f840785a8c745035e31735a077e85f8097bd
Abdullah KARACAOĞLU <abdullahkaracaoglu7@gmail.com>
2021-09-29 11:30:57 +0300

    Configure Folder Structures and index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//Reducer
+
+const app = (
+  <Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
+)
+ReactDOM.render(app, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
